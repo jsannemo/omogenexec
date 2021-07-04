65978c3091ea67b138585e01db39d006d162dc07
Johan Sannemo <jsannemo@jsannemo.se>
2021-07-04 20:27:14 +0200

    Let the permission fixer update permissions anywhere under /var/lib/omogen

--- permissionfixer/src/main.rs
+++ permissionfixer/src/main.rs
@@ -4,36 +4,40 @@ use clap::Clap;
 
 use std::{
     process::Command,
+    fs::canonicalize,
 };
 
-const SUBMISSION_PATH: &str = "/var/lib/omogen/submissions";
+const OMOGEN_PATH: &str = "/var/lib/omogen/";
 
 #[derive(Clap)]
 #[clap(name = "omogenexec-fixpermissions", about = "Resets permissions of files created by a sandbox user")]
 struct Opt {
     #[clap(long)]
-    submission_id: u32,
+    path: String,
 }
 
 fn main() {
     let opt = Opt::parse();
-    let submission_path = format!("{}{}", SUBMISSION_PATH, opt.submission_id);
+    let path = canonicalize(format!("{}{}", OMOGEN_PATH, opt.path)).unwrap();
+    if !path.starts_with(OMOGEN_PATH) {
+        panic!("Can only take ownership of files under /var/lib/omogen/")
+    }
     Command::new("/usr/bin/chattr")
         .arg("-i")
         .arg("-R")
-        .arg(&submission_path)
+        .arg(&path)
         .status()
         .expect("Failed to unmark immutability");
     Command::new("/bin/chown")
         .arg("-R")
         .arg("omogenexec-user:omogenexec-users")
-        .arg(&submission_path)
+        .arg(&path)
         .status()
         .expect("Failed to unmark immutability");
     Command::new("/bin/chmod")
         .arg("-R")
         .arg("gu+wrx")
-        .arg(&submission_path)
+        .arg(&path)
         .status()
         .expect("Failed to unmark immutability");
 }
